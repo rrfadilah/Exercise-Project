b72422aaf0aa77a61efaabb6e8bc15ac6b49d922
FizCode1 <abdulhafizemail@gmail.com>
2022-03-15 22:11:42 +0700

    initial commit

--- app/src/main/java/com/rizky/exercise_project/MainActivity.kt
+++ app/src/main/java/com/rizky/exercise_project/MainActivity.kt
@@ -2,10 +2,38 @@ package com.rizky.exercise_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Lifecycle", "Lirecycle OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "Lirecycle OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "Lirecycle OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "Lirecycle OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "Lirecycle OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "Lirecycle OnDestroy")
     }
 }
