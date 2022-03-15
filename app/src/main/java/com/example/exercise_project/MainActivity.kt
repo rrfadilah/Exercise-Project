3a3a87d5543871d4361be3a521d6eb2ecf830b1b
agungsya <agungsyahputra168@gmail.com>
2022-03-15 21:52:39 +0700

    Exercise Project

--- app/src/main/java/com/example/exercise_project/MainActivity.kt
+++ app/src/main/java/com/example/exercise_project/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.exercise_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Lifecycle", "Lifecycle OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "Lifecycle OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "Lifecycle OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "Lifecycle OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "Lifecycle OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "Lifecycle OnDestroy")
     }
 }
