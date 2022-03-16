801c2ac9bcaa40ad2a9d8667883e4a8ee92521e6
bitrock999 <72795926+bitrock999@users.noreply.github.com>
2022-03-16 20:14:31 +0700

    belum selesai

---
+++ app/src/main/java/com/rizky/exercise_project/OnBoardActivity.kt
@@ -0,0 +1,38 @@
+package com.rizky.exercise_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class OnBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board)
+        Log.d("CREATE", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("START", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("RESUME", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("PAUSE", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("STOP", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("DESTROY", "onDestroy")
+    }
+}
