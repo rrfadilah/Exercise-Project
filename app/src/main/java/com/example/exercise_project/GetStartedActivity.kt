dee7235785117c7825cbc250c35e65ec6943b120
bagussatriakusuma <logocreatorfinn@gmail.com>
2022-03-16 22:13:38 +0700

    new initial commit

--- app/src/main/java/com/example/exercise_project/GetStartedActivity.kt
+++ app/src/main/java/com/example/exercise_project/GetStartedActivity.kt
@@ -1,12 +1,13 @@
 package com.example.exercise_project
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class GetStartedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.get_started_layout)
+        setContentView(R.layout.sign_in_layout)
 
     }
 }
