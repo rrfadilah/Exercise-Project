fca9067b0d7327dceb57d093bd70ee01d30dcc81
Siti Mhus Aisyah <101188413+smaisyah@users.noreply.github.com>
2022-03-16 19:10:09 +0700

    Commit Exercise project

--- app/src/main/java/com/mhus/exercise_project/MainActivity.kt
+++ app/src/main/java/com/mhus/exercise_project/MainActivity.kt
@@ -2,10 +2,38 @@ package com.mhus.exercise_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Lifecycle","Lifecycle Oncreat")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle","Lifecycle Onstrart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle","Lifecycle Onresume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle","Lifecycle Onpause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle","Lifecycle Onstop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle","Lifecycle Ondestroy")
     }
 }
