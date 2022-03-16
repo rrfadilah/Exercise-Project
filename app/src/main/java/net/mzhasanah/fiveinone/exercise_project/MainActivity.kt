6127027380c43c1f3169dc20ee85a54ca41e364c
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2022-03-16 09:53:04 +0700

    awalan

---
+++ app/src/main/java/net/mzhasanah/fiveinone/exercise_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.mzhasanah.fiveinone.exercise_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
