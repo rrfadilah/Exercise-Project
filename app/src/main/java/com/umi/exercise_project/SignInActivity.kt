44d4d8a684691b8173d6d1a201aa904c21b49a1c
UmiNur1 <uminur.2019@student.uny.ac.id>
2022-03-16 08:31:13 -0700

    initial commit

---
+++ app/src/main/java/com/umi/exercise_project/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.umi.exercise_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
