e228c1f42e5d589ae8da2ecd1030241bc63253e8
bagussatriakusuma <logocreatorfinn@gmail.com>
2022-03-16 19:08:15 +0700

    initial commit

---
+++ app/src/main/java/com/example/exercise_project/GetStartedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.exercise_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GetStartedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.get_started_layout)
+
+    }
+}
--- app/src/main/java/com/example/exercise_project/MainActivity.kt
+++ app/src/main/java/com/example/exercise_project/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.exercise_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Lifecycle", "Lifecycle onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "Lifecycle onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "Lifecycle onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "Lifecycle onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "Lifecycle onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "Lifecycle onDestroy")
+    }
+
 }
