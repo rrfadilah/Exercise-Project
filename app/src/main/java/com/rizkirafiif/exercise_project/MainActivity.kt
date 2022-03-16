18e8d4db3bc56b86622cddcabcab7ef5c9a48917
rizkirafiif <rizkirafiif@gmail.com>
2022-03-16 18:34:52 +0700

    update onboard dan logic splash screen

--- app/src/main/java/com/rizkirafiif/exercise_project/MainActivity.kt
+++ app/src/main/java/com/rizkirafiif/exercise_project/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.rizkirafiif.exercise_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // for hiding the action bar
+        supportActionBar?.hide()
+
+        // delay for specified time
+        Handler().postDelayed({
+            val intent = Intent(this, OnBoardingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+        //Log.d("Lifecycle", "Lifecycle OnCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "Lifecycle OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "Lifecycle OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "Lifecycle OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "Lifecycle OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "Lifecycle OnDestroy")
     }
 }
