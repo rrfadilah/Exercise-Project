99506377944410efacd1485b0d229500fbe8a5f0
rizkyfadilah <skyhill.117@gmail.com>
2022-03-17 21:04:55 +0700

    implement fragment to activity implement fragment with viewpager

---
+++ app/src/main/java/com/rizky/exercise_project/galleryfragment/AdapterGallery.kt
@@ -0,0 +1,27 @@
+package com.rizky.exercise_project.galleryfragment
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+/**
+ * com.rizky.exercise_project.galleryfragment
+ *
+ * Created by Rizky Fadilah on 17/03/22.
+ * https://github.com/rizkyfadilah
+ *
+ */
+
+class AdapterGallery(
+    fragmentManager: FragmentManager,
+    val listFragment: List<Fragment>
+) :
+    FragmentStatePagerAdapter(fragmentManager) {
+    override fun getCount(): Int {
+        return listFragment.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return listFragment[position]
+    }
+}
---
+++ app/src/main/java/com/rizky/exercise_project/samplefragment/PicturesActivity.kt
@@ -0,0 +1,19 @@
+package com.rizky.exercise_project.samplefragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rizky.exercise_project.R
+
+class PicturesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pictures)
+
+        val picturesFragment = PicturesFragment()
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.add(R.id.flBottom, picturesFragment)
+        fragmentTransaction.commit()
+    }
+}
