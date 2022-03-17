b3990b175859e8af6998e4e45f92a227bd6a3220
rizkirafiif <rizkirafiif@gmail.com>
2022-03-17 13:54:29 +0700

    update styles

--- app/src/main/java/com/rizkirafiif/exercise_project/OnBoardingActivity.kt
+++ app/src/main/java/com/rizkirafiif/exercise_project/OnBoardingActivity.kt
@@ -1,11 +1,23 @@
 package com.rizkirafiif.exercise_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class OnBoardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
+
+        // for hiding the action bar
+        supportActionBar?.hide()
+
+        // just to auto slide to sign in activity
+        Handler().postDelayed({
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
