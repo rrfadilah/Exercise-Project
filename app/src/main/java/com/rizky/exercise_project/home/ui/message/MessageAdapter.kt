93ca811741f7b7dc77a573476dd98eb746fea2b2
rizkyfadilah <skyhill.117@gmail.com>
2022-03-23 22:27:45 +0700

    layout message fragment

---
+++ app/src/main/java/com/rizky/exercise_project/home/ui/message/MessageAdapter.kt
@@ -0,0 +1,12 @@
+package com.rizky.exercise_project.home.ui.message
+
+/**
+ * com.rizky.exercise_project.home.ui.message
+ *
+ * Created by Rizky Fadilah on 23/03/22.
+ * https://github.com/rizkyfadilah
+ *
+ */
+
+class MessageAdapter {
+}
---
+++ app/src/main/java/com/rizky/exercise_project/home/ui/message/MessageModel.kt
@@ -0,0 +1,16 @@
+package com.rizky.exercise_project.home.ui.message
+
+/**
+ * com.rizky.exercise_project.home.ui.message
+ *
+ * Created by Rizky Fadilah on 23/03/22.
+ * https://github.com/rizkyfadilah
+ *
+ */
+
+data class MessageModel(
+    val id: String,
+    val image: Int,
+    val name: String,
+    val lastMessage: String
+)
