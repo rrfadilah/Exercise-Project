a73cf1a906ba6e63d31730b2a474196c8742c6a2
Anastasia Kezia <anastasiakezia08@gmail.com>
2022-03-24 21:46:19 +0700

    Update message

--- app/src/main/java/com/rizky/exercise_project/MainActivity.kt
+++ app/src/main/java/com/rizky/exercise_project/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.rizky.exercise_project
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.rizky.exercise_project.menu.ui.message.MessageAdapter
+import com.rizky.exercise_project.menu.ui.message.MessageModel
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val messages: List<MessageModel> = listOf(
+            MessageModel(
+                avatar = "https://i.ibb.co/zJHYGBP/binarlogo.jpg",
+                name = "Rizky Fadilah",
+                desc = "Hallo gais semuanya disini",
+                time = "12.06",
+                badges = "100"
+            ),
+            MessageModel(
+                avatar = "https://www.its.ac.id/wp-content/uploads/2021/10/kampus-merdeka.png",
+                name = "Abdul Hafiz",
+                desc = "Mau isi aapa disini boleh aja ya teman teman semuanya",
+                time = "15.06",
+                badges = "5"
+            ),
+        )
+
+        val recyclerView: RecyclerView = findViewById(R.id.rv_message)
+
+        val adapter = MessageAdapter(messages)
+        recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/rizky/exercise_project/menu/ui/message/MessageAdapter.kt
@@ -0,0 +1,46 @@
+package com.rizky.exercise_project.menu.ui.message
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.rizky.exercise_project.R
+
+class MessageAdapter(private val list: List<MessageModel>) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Your holder should contain and initialize a member variable
+        // for any view that will be set as you render a row
+        val imageAvatar = itemView.findViewById<ImageView>(R.id.iv_img)
+        val textName = itemView.findViewById<TextView>(R.id.tv_name)
+        val textDesc = itemView.findViewById<TextView>(R.id.tv_last_message)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val view = inflater.inflate(R.layout.list_item_message, parent, false)
+        // Return a new holder instance
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get the data model based on position
+        val message: MessageModel = list[position]
+        // Set item views based on your views and data model
+        Glide.with(holder.itemView.context)
+            // .load("https://i.ibb.co/zJHYGBP/binarlogo.jpg")
+            .load(message.avatar)
+            .circleCrop()
+            .into(holder.imageAvatar)
+        holder.textName.text = message.name
+        holder.textDesc.text = message.desc
+
+    override fun getItemCount(): Int {
+        return list.count()
+    }
+}
