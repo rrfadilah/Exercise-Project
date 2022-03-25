f38fa636240ad85b508d73ad2dfbe43949f4297b
FizCode1 <abdulhafizemail@gmail.com>
2022-03-25 23:10:41 +0700

    tinggal buat data text DummyKonsul.kt setengah bold

--- app/src/main/java/com/rizky/exercise_project/data/DummyKonsul.kt
+++ app/src/main/java/com/rizky/exercise_project/data/DummyKonsul.kt
@@ -4,14 +4,25 @@ import com.rizky.exercise_project.R
 import com.rizky.exercise_project.konsultasi.KonsultasiModel
 
 object DummyKonsul {
+
+
+
     val dataKonsul: List<KonsultasiModel> = listOf(
         KonsultasiModel(
-            ic_konsultasi = R.drawable.img_dokter_umum,
-            txt_konsultasi = "Hello"
+            ic_konsultasi = R.drawable.ic_dokter_umum,
+            txt_konsultasi = "Saya butuh dokter umum"
         ),
         KonsultasiModel(
-            ic_konsultasi = R.drawable.img_dokter_umum,
-            txt_konsultasi = "Hello 2"
+            ic_konsultasi = R.drawable.ic_psikiater,
+            txt_konsultasi = "Saya butuh psikiater"
+        ),
+        KonsultasiModel(
+            ic_konsultasi = R.drawable.ic_dokter_obat,
+            txt_konsultasi = "Saya butuh dokter obat"
+        ),
+        KonsultasiModel(
+            ic_konsultasi = R.drawable.ic_dokter_umum,
+            txt_konsultasi = "Saya butuh dokter anak"
         ),
     )
 }
