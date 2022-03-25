30d6b02e84209ba80cc191fefac31a33fec6bf76
bagussatriakusuma <logocreatorfinn@gmail.com>
2022-03-25 17:24:38 +0700

    checking class & layout

---
+++ app/src/main/java/com/rizky/exercise_project/message/MessageFragment.kt
@@ -0,0 +1,20 @@
+package com.rizky.exercise_project.message
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rizky.exercise_project.databinding.FragmentDoctorBinding
+import com.rizky.exercise_project.databinding.FragmentMessageBinding
+
+class MessageFragment : Fragment() {
+    lateinit var binding: FragmentMessageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMessageBinding.inflate(layoutInflater)
+    }
+}
