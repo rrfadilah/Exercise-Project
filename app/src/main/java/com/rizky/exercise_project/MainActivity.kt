ed438c3710af2f92604c6770f28b9727eaeabb6d
bagussatriakusuma <logocreatorfinn@gmail.com>
2022-03-25 20:26:17 +0700

    commit bottomnav

--- app/src/main/java/com/rizky/exercise_project/MainActivity.kt
+++ app/src/main/java/com/rizky/exercise_project/MainActivity.kt
@@ -2,10 +2,38 @@ package com.rizky.exercise_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rizky.exercise_project.Doctor.DoctorFragment
+import com.rizky.exercise_project.databinding.ActivityMainBinding
+import com.rizky.exercise_project.hospitals.HospitalsFragment
+import com.rizky.exercise_project.message.MessageFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_for_home)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentDoctor = DoctorFragment()
+        val fragmentHospitals = HospitalsFragment()
+        val fragmentMessages = MessageFragment()
+
+        setCurrentFragment(fragmentDoctor)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navbar_Doctor -> setCurrentFragment(fragmentDoctor)
+                R.id.navbar_Messages -> setCurrentFragment(fragmentMessages)
+                R.id.navbar_Hospital -> setCurrentFragment(fragmentHospitals)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace(R.id.flFragment,fragment)
+        commit()
     }
 }
