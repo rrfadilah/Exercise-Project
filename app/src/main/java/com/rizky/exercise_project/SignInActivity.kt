3fb9522c33a0f503cc8d144fdfbf092125ce8735
rizkyfadilah <skyhill.117@gmail.com>
2022-03-30 19:34:10 +0700

    toast dan snackbar

--- app/src/main/java/com/rizky/exercise_project/SignInActivity.kt
+++ app/src/main/java/com/rizky/exercise_project/SignInActivity.kt
@@ -1,7 +1,10 @@
 package com.rizky.exercise_project
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.rizky.exercise_project.databinding.ActivitySignInBinding
 import com.rizky.exercise_project.model.Biodata
 import com.rizky.exercise_project.model.UserInfo
 
@@ -10,20 +13,34 @@ class SignInActivity : AppCompatActivity() {
         const val KEY = "KEY"
     }
 
+    lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Untuk menerima intent explicit
-        val valueFromAnotherActivity = intent.getStringExtra(Constant.Intent.EMAIL)
+//        val valueFromAnotherActivity = intent.getStringExtra(Constant.Intent.EMAIL)
 
         // Untuk menerima intent bundle
-        intent.extras?.getString(Constant.Intent.PHONE)
+//        intent.extras?.getString(Constant.Intent.PHONE)
 
         // Untuk menerima intent serializable
-        intent.extras?.getSerializable(Constant.Serialize.KEY) as Biodata
+//        intent.extras?.getSerializable(Constant.Serialize.KEY) as Biodata
 
         // Untuk menerima intent parcelize
-        intent.extras?.getParcelable<UserInfo>(Constant.Parcelize.KEY)
+//        intent.extras?.getParcelable<UserInfo>(Constant.Parcelize.KEY)
+
+
+        openPage()
+    }
+
+    fun openPage() {
+        Snackbar.make(binding.root, "Membuka Halaman Sign In", Snackbar.LENGTH_INDEFINITE)
+            .setAction("Klik Disini") {
+                // aksi yang akan kita jalan kan ketika klik di action nya...
+                Toast.makeText(this, "Membuka halaman Sign In", Toast.LENGTH_LONG).show()
+            }.show()
     }
 }
