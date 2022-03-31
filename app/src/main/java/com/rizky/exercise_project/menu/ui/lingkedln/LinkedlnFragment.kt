e1b2deae7c3ce96cdd8b500a7cd8fbd76445015d
Anastasia Kezia <anastasiakezia08@gmail.com>
2022-03-31 22:10:56 +0700

    Add feed 🚗

--- app/src/main/java/com/rizky/exercise_project/menu/ui/lingkedln/LinkedlnFragment.kt
+++ app/src/main/java/com/rizky/exercise_project/menu/ui/lingkedln/LinkedlnFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.rizky.exercise_project.databinding.FragmentDashboardBinding
 import com.rizky.exercise_project.databinding.FragmentLinkedlnBinding
-import com.rizky.exercise_project.menu.ui.dashboard.DashboardViewModel
 
 class LinkedlnFragment : Fragment() {
 
