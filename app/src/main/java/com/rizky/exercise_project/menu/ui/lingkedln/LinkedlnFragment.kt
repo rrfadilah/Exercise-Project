f4e34d0f4c8b0f57e0e4b088bb58ff0e560057a3
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-03-31 21:41:01 +0700

    Add Menu Linkedln

---
+++ app/src/main/java/com/rizky/exercise_project/menu/ui/lingkedln/LinkedlnFragment.kt
@@ -0,0 +1,44 @@
+package com.rizky.exercise_project.menu.ui.lingkedln
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.rizky.exercise_project.databinding.FragmentDashboardBinding
+import com.rizky.exercise_project.databinding.FragmentLinkedlnBinding
+import com.rizky.exercise_project.menu.ui.dashboard.DashboardViewModel
+
+class LinkedlnFragment : Fragment() {
+
+    private var _binding: FragmentLinkedlnBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(LinkedlnViewModel::class.java)
+
+        _binding = FragmentLinkedlnBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textLinkedln
+//        LinkedlnViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
