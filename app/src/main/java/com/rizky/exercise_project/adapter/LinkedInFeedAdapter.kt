39d942fe6ce7ff7c0cf0d8621cc21f9b9ee90ed1
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-04-01 00:20:46 +0700

    Revert "Merge remote-tracking branch 'origin/development/group-2' into development/group-2"
    This reverts commit ebdb14f722d82e252acb428821b2d233e5c3b6f9, reversing
    changes made to f1da7a4e433480db71f75a2462a3efe94f67fb05.

--- app/src/main/java/com/rizky/exercise_project/adapter/LinkedInFeedAdapter.kt
+++ app/src/main/java/com/rizky/exercise_project/adapter/LinkedInFeedAdapter.kt
@@ -1,24 +1,4 @@
 package com.rizky.exercise_project.adapter
 
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.rizky.exercise_project.model.LinkedInFeedModel
-
-class LinkedInFeedAdapter(private val list: List<LinkedInFeedModel>): RecyclerView.Adapter<LinkedInFeedAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+class LinkedInFeedAdapter {
 }
