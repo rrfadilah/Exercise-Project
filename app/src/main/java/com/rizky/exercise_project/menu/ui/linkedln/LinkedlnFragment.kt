63a2bcc133e6157621937a115ee154e91bbf6766
FizCode1 <abdulhafizemail@gmail.com>
2022-03-31 21:44:27 +0700

    Update hanya file LinkedIn Model dan Adapter

--- app/src/main/java/com/rizky/exercise_project/menu/ui/linkedln/LinkedlnFragment.kt
+++ app/src/main/java/com/rizky/exercise_project/menu/ui/linkedln/LinkedlnFragment.kt
@@ -1,15 +1,12 @@
-package com.rizky.exercise_project.menu.ui.lingkedln
+package com.rizky.exercise_project.menu.ui.linkedln
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.rizky.exercise_project.databinding.FragmentDashboardBinding
 import com.rizky.exercise_project.databinding.FragmentLinkedlnBinding
-import com.rizky.exercise_project.menu.ui.dashboard.DashboardViewModel
 
 class LinkedlnFragment : Fragment() {
 
