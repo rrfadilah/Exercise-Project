f1da7a4e433480db71f75a2462a3efe94f67fb05
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-03-31 22:26:11 +0700

    Add Data Dummy and Icon LinkedLn

---
+++ app/src/main/java/com/rizky/exercise_project/data/DummyLinkedlnFeed.kt
@@ -0,0 +1,27 @@
+package com.rizky.exercise_project.data
+
+import com.rizky.exercise_project.R
+import com.rizky.exercise_project.model.LinkedInFeedModel
+
+object DummyLinkedlnFeed {
+    val dataLingkedlnFeed: List<LinkedInFeedModel> = listOf(
+        LinkedInFeedModel(
+            R.drawable.dokter1,
+           "Radityo Bagus Wicaksono",
+            "3rd+",
+            "Recruitment Specialist at Hyundai Motor Company / Automotive Enthusiast/Dad"
+        ),
+        LinkedInFeedModel(
+            R.drawable.dokter2,
+            "Guru Sozuaon Simbolon",
+            "3rd+",
+            "Director | People Development | Trainer | HR Consultant | Public Speaker | Coach"
+        ),
+        LinkedInFeedModel(
+            R.drawable.dokter3,
+            "#hiring",
+            "",
+            "Topic\n 2,745,728 followers"
+        )
+    )
+}
--- app/src/main/java/com/rizky/exercise_project/model/LinkedInFeedModel.kt
+++ app/src/main/java/com/rizky/exercise_project/model/LinkedInFeedModel.kt
@@ -1,7 +1,8 @@
 package com.rizky.exercise_project.model
 
-data class LinkedInFeedModel (
+data class LinkedInFeedModel(
+    val avatar:Int,
     val nama: String,
     val tahunPengalaman: String,
     val jobList: String
-    )
+)
