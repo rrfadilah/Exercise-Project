e190961fa1fb3afc8e96e16195913e5217d3b721
Arya Rezza Anantya <43742778+anantyan@users.noreply.github.com>
2022-04-01 20:16:51 +0700

    Update Collab.kt

--- app/src/main/java/com/rizky/exercise_project/collaboration/Collab.kt
+++ app/src/main/java/com/rizky/exercise_project/collaboration/Collab.kt
@@ -2,4 +2,7 @@ package com.rizky.exercise_project.collaboration
 
 class Collab {
     val aryarezza: String? = null
+    companion object {
+        const val ARYA_REZZA: String = "ARYA_REZZA"
+    }
 }
