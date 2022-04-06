b5a9432182c75fad63dc36576194b6d36006707e
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-04-06 19:19:05 +0700

    Create Splashscreen

--- app/src/main/java/com/tegarpenemuan/myapplication/MainActivity.kt
+++ app/src/main/java/com/tegarpenemuan/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.tegarpenemuan.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OnBoardingActivity::class.java)
+            startActivity(intent)
+        }, 3000)
 
         Log.d("Lifecycle","Lifecycle OnCreate")
     }
