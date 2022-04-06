0e177584c34a310c1cfd097479b0a3464238fcb3
MasSakti <asepslow477@gmail.com>
2022-04-06 13:55:54 +0700

    Message Fragment dan Navigation

--- app/src/main/java/com/mutawalli/exercise_project/SignInActivity.kt
+++ app/src/main/java/com/mutawalli/exercise_project/SignInActivity.kt
@@ -1,30 +1,35 @@
 package com.mutawalli.exercise_project
 
 import android.content.DialogInterface
-import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Patterns
+import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import com.google.android.material.snackbar.Snackbar
 import com.mutawalli.exercise_project.databinding.ActivitySignInBinding
-import com.mutawalli.exercise_project.home.HomeActivity
 import com.mutawalli.exercise_project.model.Biodata
 import com.mutawalli.exercise_project.model.UserInfo
-import java.net.InterfaceAddress
+
 
 class SignInActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySignInBinding
-
     companion object {
         const val KEY = "KEY"
     }
 
+    lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pref = this.getSharedPreferences(Constant.Preferences.PREF_NAME, MODE_PRIVATE)
+        binding.btnSignIn.setOnClickListener {
+            sharedPreferences(pref)
+        }
 
         // Untuk menerima intent explicit
 //        val valueFromAnotherActivity = intent.getStringExtra(Constant.Intent.EMAIL)
@@ -38,73 +43,92 @@ class SignInActivity : AppCompatActivity() {
         // Untuk menerima intent parcelize
 //        intent.extras?.getParcelable<UserInfo>(Constant.Parcelize.KEY)
 
-        binding.tvForgotPassword.setOnClickListener {
-            Toast.makeText(this, "Lupa Password di pencet", Toast.LENGTH_SHORT).show()
-        }
-
-        binding.btnSignIn.setOnClickListener {
-            val email = binding.etEmail.text.toString()
-            val pass = binding.etPassword.text.toString()
-
-            if (email.isEmpty()) {
-                Toast.makeText(this, "Kolom email masih kosong", Toast.LENGTH_SHORT).show()
-            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                Toast.makeText(this, "Email tidak valid", Toast.LENGTH_SHORT).show()
-            } else if (pass.isEmpty()) {
-                Toast.makeText(this, "Kolom password masih kosong", Toast.LENGTH_SHORT).show()
-            } else if (pass.length < 8) {
-                Toast.makeText(this, "Minimum 8 Characters", Toast.LENGTH_SHORT).show()
-            } else if (!pass.matches(".*[A-Z].*".toRegex())) {
-                Toast.makeText(this, "Password harus mengandung 1 Upper-case", Toast.LENGTH_SHORT)
-                    .show()
-            } else if (!pass.matches(".*[a-z].*".toRegex())) {
-                Toast.makeText(this, "Password harus mengandung 1 Lower-case", Toast.LENGTH_SHORT)
-                    .show()
-            }
-            return@setOnClickListener
-        }
-
-        binding.tvCreateNewAccount.setOnClickListener {
-            Toast.makeText(this, "Create new account di pencet", Toast.LENGTH_SHORT).show()
-        }
-
-        openPage()
-
-//        val dialog = AlertDialog.Builder(this)
-//        dialog.setTitle("Judul Dialog")
-//
-//        dialog.setMessage("Isi Pesan Dialog")
-//        dialog.setCancelable(true)
-//        dialog.show()
-
-
-        val dialogAction = AlertDialog.Builder(this)
-        dialogAction.setTitle("Judul Dialog")
-        dialogAction.setMessage("Pesan Dialog")
-        dialogAction.setPositiveButton("Positif", object : DialogInterface.OnClickListener {
-            override fun onClick(dialog: DialogInterface?, which: Int) {
-                dialog?.dismiss()
-            }
-        })
-        dialogAction.setNegativeButton("Negatif") { dialog, which ->
-            dialog?.dismiss()
-        }
-        dialogAction.setNeutralButton("Netral") { dialog, which ->
-            dialog?.dismiss()
-        }
-        dialogAction.show()
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        Toast.makeText(this, getString(R.string.toast_text), Toast.LENGTH_LONG).show()
     }
 
     fun openPage() {
-        Snackbar.make(binding.root, "Membuka halaman Sign In", Snackbar.LENGTH_INDEFINITE)
+        Snackbar.make(binding.root, "Membuka Halaman Sign In", Snackbar.LENGTH_INDEFINITE)
             .setAction("Klik Disini") {
+                // aksi yang akan kita jalan kan ketika klik di action nya...
                 Toast.makeText(this, "Membuka halaman Sign In", Toast.LENGTH_LONG).show()
             }.show()
     }
 
+    fun dialogStandard() {
+        // Dialog standard
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("judul dialog")
+        dialog.setMessage("isi pesan dialog")
+        dialog.setCancelable(true)
+        dialog.show()
+    }
+
+    fun dialogAction() {
+        // dialog dengan action
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("judul dialog")
+        dialog.setMessage("isi pesan dialog")
+        dialog.setPositiveButton("POsitif", object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                dialog?.dismiss()
+            }
+        })
+        dialog.setNegativeButton("Negatif") { dialog, which ->
+            dialog.dismiss()
+        }
+        dialog.setNeutralButton("Netral") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    fun dialogCustomLayout() {
+        val view = LayoutInflater.from(this).inflate(R.layout.dialog_custom, null, false)
+        val dialog = AlertDialog.Builder(this)
+        dialog.setView(view)
+        dialog.create().show()
+    }
+
+    fun dialogWithFragment() {
+        val dialog = CustomDialogFragment()
+        dialog.show(supportFragmentManager, null)
+    }
+
+    // contoh pengguanan sharedpreferences di dalam sign in
+    fun sharedPreferences(pref: SharedPreferences) {
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        // untuk melakukan penyimpanan ke sharedpreferences hanya mengguanakn code berikut ini
+        pref.edit {
+            putString(Constant.Preferences.KEY.EMAIL, email)
+            putString(Constant.Preferences.KEY.PASSWORD, password)
+            apply()
+        }
+
+        // untuk mengambil data dari shared preferences menggunakan code berikut ini
+        val prefEmail = pref.getString(Constant.Preferences.KEY.EMAIL, "")
+        val prefPassword = pref.getString(Constant.Preferences.KEY.PASSWORD, "")
+        Snackbar.make(
+            binding.root,
+            "Value yang tersimpan adalah berikut: Email: $prefEmail dan Password: $prefPassword",
+            Snackbar.LENGTH_INDEFINITE
+        ).show()
+    }
+
+    // contoh satu lagi pengguanaan shared preferences untuk konfigurasi aplikasi
+    fun sharedPrefDarkMode(pref: SharedPreferences, darkMode: Boolean) {
+        pref.edit {
+            putBoolean(Constant.Preferences.KEY.DARK_MODE, darkMode)
+        }
+        pref.getBoolean(Constant.Preferences.KEY.DARK_MODE, false)
+    }
+
+    // contoh satu lagi pengguanaan shared preferences untuk konfigurasi aplikasi
+    fun sharedPrefLanguage(pref: SharedPreferences, language: String) {
+        pref.edit {
+            putString(Constant.Preferences.KEY.APP_LANGUAGE, language)
+        }
+        pref.getString(Constant.Preferences.KEY.APP_LANGUAGE, "id")
+    }
 }
